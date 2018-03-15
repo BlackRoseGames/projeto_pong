fc1a8c1a8edc096b01ae8d09d7e7dd8faac7979e
Antonio Marco da Silva <antomarsi@hotmail.com>
2018-03-15 20:32:23 -0300

    Reescrito todo sistema

--- scenes/ball/ball.gd
+++ scenes/ball/ball.gd
@@ -2,14 +2,21 @@ extends RigidBody2D
 
 export (float) var MIN_SPEED # minimum speed range
 export (float) var MAX_SPEED # maximum speed range
-var velocity = 300
+var velocity = 400
 var canMove = true
 var direction = Vector2(-1,0.5)
 
 onready var initial_pos = self.position
 
 func reset():
-	set_fixed_process(true)
+	pass
 
 func _ready():
-    linear_velocity = direction*velocity
+	linear_velocity = direction*velocity
+
+func _on_Ball_body_entered(body):
+	var name = body.get_name()
+	print(name)
+	if name == "left" or name == "right":
+		linear_velocity *= 1.025
+		print(linear_velocity)
